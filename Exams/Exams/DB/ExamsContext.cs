e464dcfa0201c436b65c1cfadf6dc521aaf53d95
Anna Petrova <anna5437743@mail.ru>
2024-11-11 23:58:41 +0300

    Added dbContext, dbInitializer, Migrations

---
+++ Exams/Exams/DB/ExamsContext.cs
@@ -0,0 +1,46 @@
+﻿using Exams.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exams.DB
+{
+    public class ExamsContext : DbContext
+    {
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<ExamResult> ExamResults { get; set; }
+
+        public ExamsContext(DbContextOptions<ExamsContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasOne(s => s.Group)
+                .WithMany(g => g.Students)
+                .HasForeignKey(s => s.GroupId);
+
+            modelBuilder.Entity<Teacher>()
+                .HasMany(t => t.Subjects)
+                .WithMany(s => s.Teachers);
+
+            modelBuilder.Entity<ExamResult>()
+                .HasOne(e => e.Student)
+                .WithMany(s => s.ExamResults)
+                .HasForeignKey(e => e.StudentId);
+
+            modelBuilder.Entity<ExamResult>()
+                .HasOne(e => e.Teacher)
+                .WithMany()
+                .HasForeignKey(e => e.TeacherId);
+
+            modelBuilder.Entity<ExamResult>()
+                .HasOne(e => e.Subject)
+                .WithMany()
+                .HasForeignKey(e => e.SubjectId);
+        }
+    }
+}
