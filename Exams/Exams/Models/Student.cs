b1060675fb89b8a16cc5835f609e4027224dd15c
Anna Petrova <anna5437743@mail.ru>
2024-11-11 22:51:17 +0300

    Added models for entityframework tables

---
+++ Exams/Exams/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Exams.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+    }
+}
---
+++ Exams/Exams/Models/Subject.cs
@@ -0,0 +1,9 @@
+﻿namespace Exams.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Teacher> Teachers { get; set; } = new List<Teacher>();
+    }
+}
